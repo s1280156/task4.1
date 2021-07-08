9a0093e89b717d1d41a0262f6f7ea221ade95934
s1280156 <86813685+s1280156@users.noreply.github.com>
2021-07-08 19:57:03 +0900

    Greetings added

---
+++ DiceGame(1).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void){
+        int i;
+        int sum=0;
+        char name[30];
+        srand(time(NULL));
+
+        printf("What is your name?\n");
+        scanf("%s", name);
+        printf("Hello,%s!\n", name);
+        for(i=0;i<2;i++){
+                int r=rand();
+                printf("Die %d: %d\n",i+1,(r%6)+1);
+                sum+=(r%6)+1;
+        }
+        printf("Total value: %d\n",sum);
+        return 0;
+}
